6cb526ae9d094e6dba06b4dc94957bb591114470
hy299792458 <hy299792458@gmail.com>
2017-06-26 22:49:48 -0700

    daily

---
+++ 371-sumOfTwo.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        MAX_INT = 0x7FFFFFFF
+        MIN_INT = 0x80000000
+        MASK = 0x100000000
+        while b:
+            a, b = (a ^ b) % MASK, ((a & b) << 1) % MASK
+        return a if a <= MAX_INT else ~((a % MIN_INT) ^ MAX_INT)
